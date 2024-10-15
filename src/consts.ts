2956b0f7856f86bc1fbac56c5afdc513ecb9daaf
polyglot-chameleon <oleg.harlamov@web.de>
2024-10-15 11:05:05 +0200

    change constant site data #1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Dr. Leandros";
+export const SITE_DESCRIPTION = "This and that.";
